c835f789b2d14c32e05f861f80e3dc7d86670184
haochiencho@gmail.com <haochiencho@gmail.com>
2017-02-01 22:53:12 -0800

    create.sql and drop.sql

--- drop.sql
+++ drop.sql
@@ -1,17 +1,7 @@
-IF OBJECT_ID('category', 'U') IS NOT NULL
-DROP TABLE category;
+DROP TABLE IF EXISTS `bid`;
+DROP TABLE IF EXISTS `bidder`;
+DROP TABLE IF EXISTS `category`;
+DROP TABLE IF EXISTS `item`;
+DROP TABLE IF EXISTS `location`;
+DROP TABLE IF EXISTS `seller`;
 
-IF OBJECT_ID('location', 'U') IS NOT NULL
-DROP TABLE location;
-
-IF OBJECT_ID('seller', 'U') IS NOT NULL
-DROP TABLE seller;
-
-IF OBJECT_ID('bidder', 'U') IS NOT NULL
-DROP TABLE bidder;
-
-IF OBJECT_ID('item', 'U') IS NOT NULL
-DROP TABLE item;
-
-IF OBJECT_ID('bid', 'U') IS NOT NULL
-DROP TABLE bid;
