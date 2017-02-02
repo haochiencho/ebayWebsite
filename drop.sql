76d3ea5bc2851fe2e9d913305ab6b09af74e6080
JoannePark <jpark.per3@gmail.com>
2017-02-01 21:40:28 -0800

    started drop.sql

---
+++ drop.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID('category', 'U') IS NOT NULL
+DROP TABLE category;
+
+IF OBJECT_ID('location', 'U') IS NOT NULL
+DROP TABLE location;
+
+IF OBJECT_ID('seller', 'U') IS NOT NULL
+DROP TABLE seller;
+
+IF OBJECT_ID('bidder', 'U') IS NOT NULL
+DROP TABLE bidder;
+
+IF OBJECT_ID('item', 'U') IS NOT NULL
+DROP TABLE item;
+
+IF OBJECT_ID('bid', 'U') IS NOT NULL
+DROP TABLE bid;
