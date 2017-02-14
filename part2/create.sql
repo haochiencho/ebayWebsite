8c29f13b7f30f30161612708f4a6b8b14b6f7246
haochiencho@gmail.com <haochiencho@gmail.com>
2017-02-13 23:15:07 -0800

    created categoryList relation

--- part2/create.sql
+++ part2/create.sql
@@ -4,3 +4,4 @@ CREATE TABLE bidder(bidderID VARCHAR(512) PRIMARY KEY, rating int, locationID in
 CREATE TABLE item (itemID int PRIMARY KEY, name VARCHAR(256), currently numeric(15,2), buyPrice numeric(15,2), firstBid numeric(15,2), numberOfBids int, locationID int, started TIMESTAMP, ends TIMESTAMP, sellerID VARCHAR(128), description VARCHAR(4000));
 CREATE TABLE category(itemID int, category VARCHAR(128), PRIMARY KEY(itemID, category));
 CREATE TABLE bid(itemID int, bidderID VARCHAR(128), bidID int PRIMARY KEY, bidTime TIMESTAMP, amount int);
+CREATE TABLE categoryList(itemID int PRIMARY KEY, categoryList VARCHAR(512));
