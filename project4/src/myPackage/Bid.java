359b91308d65bc67efeb6f09855dd4d904187287
JoannePark <jpark.per3@gmail.com>
2017-03-03 14:19:47 -0800

    add Bid class

---
+++ project4/src/myPackage/Bid.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Bid {
+    public Bid(String bidderID, String bidderRating, String time, String amount) {
+        this.bidderID = bidderID;
+        this.bidderRating = bidderRating;
+        this.time = time;
+        this.amount = amount;
+    }
+
+	//TODO: make member variables private, and use getters/setters in itemServlet+itemDataParser
+    public String bidderID;
+    public String bidderRating;
+    public String time;
+    public String amount; 
+}
